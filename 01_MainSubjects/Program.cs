adfcb65694a9bc3d0e9d64927c7b1b1df35339e4
mehmmetcann <mehmetcan_3566@hotmail.com>
2025-11-28 18:35:06 +0300

    Add files via upload

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region String Değişkenler
+            // string name;
+            // name = "Mehmetcan";
+            // Console.Write(name);
+
+
+
+
+            //string customerName;
+            //string customerSurname;
+            //string customerEmail;
+            //string customerPhone, district, city;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "+90 552 390 547";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("----------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + "/" + city);
+            //Console.WriteLine("----------------------------");
+            //Console.WriteLine($"my name is {customerName} and my surname is {customerSurname}");
+            //Console.WriteLine("my name is {0} and my surname is {1}", customerName, customerSurname);
+            #endregion
+
+
+
+
+            #region Int Değişkenler
+            //int number = 24;
+            //Console.WriteLine(number);
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("**** Restoran Menü Fiyatı");
+            Console.WriteLine();
+            Console.WriteLine("-----Hamburger: " + cokePrice+ "TL");
+            Console.WriteLine("-----Pizza: " + pizzaPrice+"TL");
+            Console.WriteLine("-----Kola: " + cokePrice+"TL");
+            Console.WriteLine("-----Limonata: " + lemonadePrice + "TL");
+            Console.WriteLine("-----Kızartma: " + friesPrice + "TL");
+            Console.WriteLine("-----Su: " + waterPrice + "TL");
+            Console.WriteLine();
+
+            Console.WriteLine("**** Restoran Menü Fiyatı");
+            Console.WriteLine();
+            int hamburgerCount = 3;
+            int cokeCount = 3;
+            int waterCount = 3;
+            int friesCount = 1;
+            int pizzaCount = 0;
+            int lemonadeCount = 0;
+
+            int totalHamburgerPrice = 0;
+            int totalCokePrice = 0;
+            int totalWaterPrice = 0;
+            int totalFriesPrice = 0;
+            int totalPizzaPrice = 0;
+            int totalLemonadePrice = 0;
+            int totalPrice =0;
+
+            totalHamburgerPrice = hamburgerCount * hamburgerPrice;
+            totalCokePrice = cokeCount * cokePrice;
+            totalWaterPrice = waterCount * waterPrice;
+            totalFriesPrice = friesCount * friesPrice;
+            totalPizzaPrice= pizzaCount * pizzaPrice;
+            totalLemonadePrice= lemonadeCount * lemonadePrice;
+            totalPrice = totalHamburgerPrice + totalCokePrice + totalWaterPrice + totalFriesPrice + totalPizzaPrice + totalLemonadePrice;
+
+
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Hamburger Tutarı: " + totalHamburgerPrice + "TL");
+            Console.WriteLine("Pizza Tutarı: " + totalPizzaPrice + "TL");
+            Console.WriteLine("Kızartma Tutarı: " + totalFriesPrice + "TL");
+            Console.WriteLine("Kola Tutarı: " + totalCokePrice + "TL");
+            Console.WriteLine("Limonata Tutarı: " + totalLemonadePrice + "TL");
+            Console.WriteLine("Su Tutarı: " + totalWaterPrice + "TL");
+            Console.WriteLine();
+            Console.WriteLine("Toplam Tutar: " + totalPrice + "TL");
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
